3271ce3fb7322baa146fed73cbbb8b29299f0d60
itchybarn <itchybarn2@gmail.com>
2025-12-15 12:25:42 -0500

    start_systemd.sh now copies portfolio.service to the correct folder

--- scripts/start_systemd.sh
+++ scripts/start_systemd.sh
@@ -4,7 +4,8 @@
 set -e
 
 echo "---Starting systemd setup---"
- 
+
+sudo cp /home/ec2-user/portfolio/portfolio.service /etc/systemd/system/portfolio.service
 sudo systemctl daemon-reload
 sudo systemctl enable portfolio.service
 sudo systemctl start portfolio.service
