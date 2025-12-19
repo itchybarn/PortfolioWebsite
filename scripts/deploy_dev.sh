de931e22821772820f952ac38e6bd6c22a35d2a6
itchybarn <itchybarn2@gmail.com>
2025-12-19 18:35:13 -0500

    Script was accidentally Scripte

--- scripts/deploy_dev.sh
+++ scripts/deploy_dev.sh
@@ -3,6 +3,6 @@
 set -e
 
 RESTART="${RESTART:-false}"
-SCRIPT="${SCRIPTE:-deploy.sh}"
+SCRIPT="${SCRIPT:-deploy.sh}"
 
 ssh dev "cd /home/ec2-user/portfolio/scripts && ENV=dev RESTART=$RESTART ./$SCRIPT"
