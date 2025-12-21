46cb7140c8d573a957d97b23b5f82d5b1b5f1391
itchybarn <itchybarn2@gmail.com>
2025-12-21 14:31:00 -0500

    Small change to systemd script

--- scripts/start_systemd.sh
+++ scripts/start_systemd.sh
@@ -5,9 +5,12 @@ set -e
 
 echo "---Starting systemd setup---"
 
+source "env.sh"
+
 sudo cp /home/ec2-user/portfolio/portfolio.service /etc/systemd/system/portfolio.service
 
 # load the bootstrap configuration file to redirect to HTTPS until the site is fully up and running
+
 sudo cp /home/ec2-user/portfolio/nginx/portfolio_bootstrap.conf /etc/nginx/conf.d/portfolio.conf
 sudo systemctl enable --now nginx
 sudo nginx -t
