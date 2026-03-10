8427b727087d8f1e87cb67eff876c754829a9200
itchybarn <itchybarn2@gmail.com>
2026-03-10 11:09:34 -0400

    Attempted to fix the background-blend-mode problem with a GitHub resource, now linked in the readme

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,18 @@
-import ScrollingBackground from "./components/ScrollingBackground"
-import backgroundTexture from "./assets/checkerboard.svg"
+import ScrollingBackground from "./components/ScrollingBackground";
+import backgroundTexture from "./assets/checkerboard.svg";
 
 function App() {
   return (
     <>
-      <ScrollingBackground texture={backgroundTexture} tileSize={500} xSpeed={15} ySpeed={-40} color="#232d3d"/>
+      <ScrollingBackground
+        texture={backgroundTexture}
+        tileSize={500}
+        xSpeed={15}
+        ySpeed={-40}
+        color="rgb(44, 47, 94)"
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/components/ScrollingBackground.tsx
+++ frontend/src/components/ScrollingBackground.tsx
@@ -1,4 +1,4 @@
-import "../css/ScrollingBackground.css"
+import "../css/ScrollingBackground.css";
 
 interface Props {
   texture: string;
@@ -8,22 +8,27 @@ interface Props {
   color?: string;
 }
 
-const ScrollingBackground = ({ texture, tileSize, xSpeed=5, ySpeed=5, color="transparent" }: Props) => {
+const ScrollingBackground = ({
+  texture,
+  tileSize,
+  xSpeed = 5,
+  ySpeed = 5,
+  color = "rgba(255, 255, 255, 0)",
+}: Props) => {
   return (
     <div
       className="scrolling-bg"
       style={{
         backgroundImage: `url(${texture})`,
-        backgroundColor: color,
-        backgroundBlendMode: "multiply",
         backgroundSize: `${tileSize}px ${tileSize}px`,
+        ["--color" as string]: color,
         ["--tile-w" as string]: `${Math.sign(xSpeed) * tileSize}px`,
         ["--tile-h" as string]: `${Math.sign(ySpeed) * tileSize}px`,
         ["--speed-x" as string]: `${Math.abs(xSpeed)}s`,
         ["--speed-y" as string]: `${Math.abs(ySpeed)}s`,
       }}
     />
-  )
-}
+  );
+};
 
-export default ScrollingBackground
+export default ScrollingBackground;
