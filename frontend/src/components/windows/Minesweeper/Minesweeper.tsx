6213f38d5cfbfc2c4ac6f20a004109d90da9fb88
Jack Wagenheim <wagenheimj@moravian.edu>
2026-03-30 12:55:03 -0400

    Added sliders for mine chance and board size

--- frontend/src/components/windows/Minesweeper/Minesweeper.tsx
+++ frontend/src/components/windows/Minesweeper/Minesweeper.tsx
@@ -1,16 +1,47 @@
 import Board from "./Board";
 import "@src/css/windows/MinesweeperGame.css";
 import NewGameButton from "./NewGameButton";
+import { useRef, useState } from "react";
 
 interface Props {
   className: string;
 }
 
+const MIN_BOARD_SIZE = 5;
+const MAX_BOARD_SIZE = 20;
+const DEFAULT_BOARD_SIZE = 10;
+
+const MIN_MINE_CHANCE = 10;
+const MAX_MINE_CHANCE = 30;
+const DEFAULT_MINE_CHANCE = 18;
+
 const Minesweeper = ({ className }: Props) => {
+  const [boardSize, setBoardSize] = useState(DEFAULT_BOARD_SIZE);
+  const [mineChance, setMineChance] = useState(DEFAULT_MINE_CHANCE);
+  const [gameId, setGameId] = useState(0);
+
   return (
     <div className={className}>
-      <NewGameButton />
-      <Board />
+      <div className="option-header">
+        <input
+          type="range"
+          min={MIN_BOARD_SIZE}
+          max={MAX_BOARD_SIZE}
+          value={boardSize}
+          onChange={(e) => setBoardSize(Number(e.target.value))}
+          step={1}
+        />
+        <NewGameButton onNewGame={() => setGameId(id => id + 1)}/>
+        <input
+          type="range"
+          min={MIN_MINE_CHANCE}
+          max={MAX_MINE_CHANCE}
+          value={mineChance}
+          onChange={(e) => setMineChance(Number(e.target.value))}
+          step={1}
+        />
+      </div>
+      <Board key={gameId} size={{ x: boardSize, y: boardSize }} mineChance={mineChance} />
     </div>
   );
 };
--- frontend/src/components/windows/Minesweeper/NewGameButton.tsx
+++ frontend/src/components/windows/Minesweeper/NewGameButton.tsx
@@ -2,9 +2,13 @@ import SmileImage from "@src/assets/Minesweeper/minesweeper-smile.svg"
 import CellImage from "@src/assets/Minesweeper/minesweeper-block.svg"
 import "@src/css/windows/MinesweeperGame.css"
 
-const NewGameButton = () => {
+interface NewGameButtonProps {
+    onNewGame: () => void;
+}
+
+const NewGameButton = ({ onNewGame }: NewGameButtonProps) => {
   return (
-    <button className="new-game-button">
+    <button className="new-game-button" onClick={onNewGame}>
         <img src={CellImage} className="cell-background" />
         <img src={SmileImage} className="cell-overlay"/>
     </button>
